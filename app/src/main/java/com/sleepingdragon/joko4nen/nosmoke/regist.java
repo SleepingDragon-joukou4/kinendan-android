d6a98dadec4900a9aca075162185a325395a95bb
hayashi <highen4.wtf@gmail.com>
2015-06-09 13:01:17 +0900

    asd

--- app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/regist.java
+++ app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/regist.java
@@ -10,12 +10,17 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Spinner;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import java.util.ArrayList;
 
 public class regist extends Activity implements OnClickListener {
 
     private Button inext;
-    private Spinner nSpinner;
-    private String spinnerItems[] = {"Spinner", "Spinner 1", "Spinner 2", "Spinner 3"};
+    private Spinner rspn;
+    private String rspnitem[] = {"Spinner", "Spinner 1", "Spinner 2", "Spinner 3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +30,31 @@ public class regist extends Activity implements OnClickListener {
         inext = (Button) findViewById(R.id.invite_next);
         inext.setOnClickListener(this);
 
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("‚è‚ã‚¤");
+        list.add("‚Ö‚Ñ");
+        list.add("‚¤‚Ü");
+        list.add("‚Ð‚Â‚¶");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = (Spinner) findViewById(R.id.tabakospn);
+        spinner.setAdapter(adapter);
     }
+
+
 
             public void onClick(View v) {
 
                 if (v == inext) {
                     Intent intent = new Intent(this, team_create.class);
-                    startActivityForResult(intent,0);
+                    startActivityForResult(intent, 0);
 
 
+                }
             }
         }
-    }
 
 
