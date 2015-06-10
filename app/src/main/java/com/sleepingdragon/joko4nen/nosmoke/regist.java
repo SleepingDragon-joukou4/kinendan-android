b12bc3a2aeaff4dfc102e7234610bf6ab8ba3325
hayashi <highen4.wtf@gmail.com>
2015-06-10 12:14:17 +0900

    regist.java 作成中

--- app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/regist.java
+++ app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/regist.java
@@ -2,45 +2,64 @@ package com.sleepingdragon.joko4nen.nosmoke;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.TextView;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Spinner;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import java.util.ArrayList;
+import android.widget.EditText;
 
 public class regist extends Activity implements OnClickListener {
 
     private Button inext;
-   
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.regist);
 
-        inext = (Button) findViewById(R.id.invite_next);
-        inext.setOnClickListener(this);
 
+
+        //すぴなーの中身セット　本来はDBから取得
         ArrayList<String> list = new ArrayList<String>();
         list.add("りゅう");
         list.add("へび");
         list.add("うま");
         list.add("ひつじ");
 
+
+        //スピナーのデザイン指定
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner tabakosyurui = (Spinner) findViewById(R.id.tabakospn);
+        tabakosyurui.setAdapter(adapter);
 
-        Spinner spinner = (Spinner) findViewById(R.id.tabakospn);
-        spinner.setAdapter(adapter);
+        //すぴなーで選択されたアイテムを取得
+        //int idx = tabakosyurui.getSelectedItemPosition();
+        //tabakodbをDBへ登録
+        String tabakodb = (String)tabakosyurui.getSelectedItem();
+
+
+        //テキストボックス指定
+        EditText regnametxt = (EditText) findViewById(R.id.reg_nametext);
+        //テキストボックスreg_nametextの中身がstrの中へ
+        //次へボタンが押されたら、usernameをDBへ登録
+        String username = regnametxt.getText().toString();
+
+        //もう一つのテキストボックスの取得
+        EditText tabakohonsu = (EditText)findViewById(R.id.reg_tabakotext);
+        String syokihonsu = tabakohonsu.getText().toString();
+
+
+        //進むボタン
+        inext = (Button) findViewById(R.id.invite_next);
+        inext.setOnClickListener(this);
+
+
+
     }
 
 
@@ -48,12 +67,18 @@ public class regist extends Activity implements OnClickListener {
             public void onClick(View v) {
 
                 if (v == inext) {
+                    //次へボタン押したときの処理　たぶんDBへ挿入するSQLもここ
                     Intent intent = new Intent(this, team_create.class);
                     startActivityForResult(intent, 0);
 
 
                 }
             }
+
+
+
+
+
         }
 
 
