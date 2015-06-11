613bcaea07fc9592e9a814b54398e74c218642af
Ryu Hanaoka <doradora56@gmail.com>
2015-06-11 11:00:42 +0900

    チーム設定作成機能

---
+++ app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/teamsetting/TeamSettingActivity.java
@@ -0,0 +1,20 @@
+package com.sleepingdragon.joko4nen.nosmoke.teamsetting;
+
+import com.sleepingdragon.joko4nen.nosmoke.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+/**
+ * Created by ryu on 15/06/11.
+ */
+public class TeamSettingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teamsetting);
+    }
+}
