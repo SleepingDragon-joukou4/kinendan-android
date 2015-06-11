107b381460e072eead960f9c656c5b24a514876a
Ryu Hanaoka <doradora56@gmail.com>
2015-06-11 13:41:33 +0900

    teamsetting　マニフェストに追加

--- app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/teamsetting/TeamSettingActivity.java
+++ app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/teamsetting/TeamSettingActivity.java
@@ -6,15 +6,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 
 /**
  * Created by ryu on 15/06/11.
  */
 public class TeamSettingActivity extends Activity {
+
+    EditText teamNameText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teamsetting);
+
+        teamNameText = (EditText)findViewById(R.id.team_nametext);
+        
+
     }
 }
