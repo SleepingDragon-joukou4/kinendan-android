b39193e490dba72b6e1366257d6f8f4dc93c8261
hayashi <highen4.wtf@gmail.com>
2015-06-18 10:22:17 +0900

    Revert "no message"
    This reverts commit 197dd6c97d66825556f5f2aad1a65bcbeaf6e123.

--- app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/teamsetting/TeamSettingActivity.java
+++ app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/teamsetting/TeamSettingActivity.java
@@ -30,25 +30,36 @@ public class TeamSettingActivity extends Activity {
         teamNameText = (EditText) findViewById(R.id.team_nametext);
         insertteamname = teamNameText.getText().toString();
 
+<<<<<<< HEAD
         back = (Button) findViewById(R.id.teamset_back);
+=======
         back = (Button)findViewById(R.id.teamset_back );
+>>>>>>> 3d810c5b24da5c882e3c98059a5cb63e83a60301
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+<<<<<<< HEAD
+=======
                 backpage();
 
+>>>>>>> 3d810c5b24da5c882e3c98059a5cb63e83a60301
             }
         });
 
 
     }
+<<<<<<< HEAD
 
-
+    public void onClick(View v) {
+        if (v == back) {
+        }
+=======
     public void backpage(){
 
         Intent goTeamCreate = new Intent(this,Team_createActivity.class);
         startActivityForResult(goTeamCreate,0);
 
+>>>>>>> 3d810c5b24da5c882e3c98059a5cb63e83a60301
     }
 }
