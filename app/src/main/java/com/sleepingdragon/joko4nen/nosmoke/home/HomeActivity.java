10950e1db1de1228b07e3dc90219303ca8495c9f
Ryu Hanaoka <doradora56@gmail.com>
2015-06-25 23:24:45 +0900

    [Refactor] 画面遷移の繰り返し部分
    ButtterKinfeで短いコード量にしました。

--- app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/home/HomeActivity.java
+++ app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/home/HomeActivity.java
@@ -3,7 +3,6 @@ package com.sleepingdragon.joko4nen.nosmoke.home;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import com.sleepingdragon.joko4nen.nosmoke.R;
@@ -11,62 +10,46 @@ import com.sleepingdragon.joko4nen.nosmoke.ranking.RankingActivity;
 import com.sleepingdragon.joko4nen.nosmoke.schedule.ScheduleActivity;
 import com.sleepingdragon.joko4nen.nosmoke.syohin.SyohinActivity;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 /**
  * Created by Ryosei on 2015/06/18.
  */
 public class HomeActivity extends Activity {
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-
-        Button HomeButton = (Button) findViewById(R.id.HomeButton);
-        Button RankingButton = (Button) findViewById(R.id.RankingButton);
-        Button ScheduleButton = (Button) findViewById(R.id.ScheduleButton);
-        Button SyohinButton = (Button) findViewById(R.id.SyohinButton);
-
-
-        //HomeButtonが押された場合Home画面に遷移
-        HomeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,
-                        HomeActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        //RankingButtonが押された場合Ranking画面に遷移
-        RankingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,
-                        RankingActivity.class);
-                startActivity(intent);
-            }
-        });
-        //ScheduleButtonが押された場合schedule画面に遷移
-        ScheduleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,
-                        ScheduleActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        //SyohinButtonが押された場合syohin画面に遷移
-        SyohinButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,
-                        SyohinActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        ButterKnife.inject(this);
     }
 
+    @OnClick({R.id.HomeButton,R.id.ScheduleButton,
+            R.id.RankingButton, R.id.SyohinButton})
+    void onClick(Button button){
+        switch (button.getId()) {
+            case R.id.HomeButton:
+                startActivity(new Intent(HomeActivity.this,
+                        HomeActivity.class));
+                break;
+            case R.id.RankingButton:
+                startActivity(new Intent(HomeActivity.this,
+                        RankingActivity.class));
+                break;
+            case R.id.ScheduleButton:
+                startActivity(new Intent(HomeActivity.this,
+                        ScheduleActivity.class));
+                break;
+            case R.id.SyohinButton:
+                startActivity(new Intent(HomeActivity.this,
+                        SyohinActivity.class));
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
 }
 
 
