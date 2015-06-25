b169d48d69e88594c05a4a858b49e15dabf0c179
Ryu Hanaoka <doradora56@gmail.com>
2015-06-26 00:29:40 +0900

    [追加] Javadoc形式のドキュメント追加
    振る舞いを明記

--- app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/util/network/HomeSelectConnectionService.java
+++ app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/util/network/HomeSelectConnectionService.java
@@ -5,21 +5,36 @@ import org.json.JSONArray;
 
 /**
  * Created by ryu on 15/06/25.
+ * Home画面での通信の操作を扱うクラス
+ *
+ * @author ryu
+ * {@link APIConnectionService} 継承元
  */
 public class HomeSelectConnectionService extends APIConnectionService {
     private final String HOME_SELECT_URL = BASE_URL + "homeselect";
-    private final String USERID = "&UserId=";
-    private final String TEAMID = "&TeamId=";
+    private final String QUERY_USERID = "&UserId=";
+    private final String QUERY_TEAMID = "&TeamId=";
 
     private String useId = "";
     private String teamId = "";
-    private String url = HOME_SELECT_URL + USERID + useId + TEAMID + teamId;
+    private String url = HOME_SELECT_URL + QUERY_USERID + useId + QUERY_TEAMID + teamId;
 
+    /**
+     * コンストラクタ
+     *
+     * @param useId ユーザーID
+     * @param teamId チームID
+     */
     HomeSelectConnectionService(String useId, String teamId){
         this.useId = useId;
         this.teamId = teamId;
     }
 
+    /**
+     * ホーム画面に取得した情報を設定
+     *
+     * {@link com.sleepingdragon.joko4nen.nosmoke.util.network.APIConnectionService.ConnectionListener}
+     */
     public void settingHome(){
         request(url,new ConnectionListener(){
 
