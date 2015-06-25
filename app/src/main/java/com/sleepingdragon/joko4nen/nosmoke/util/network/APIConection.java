6b7758e31c4c0787fcc15b25f56d888b36826527
Ryu Hanaoka <doradora56@gmail.com>
2015-06-25 16:58:48 +0900

    API 接続　リスナー実装開始

--- app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/util/network/APIConection.java
+++ app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/util/network/APIConection.java
@@ -1,6 +1,12 @@
 package com.sleepingdragon.joko4nen.nosmoke.util.network;
 
 
+import com.sleepingdragon.joko4nen.nosmoke.URLConnectionAsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 
 /**
@@ -17,6 +23,10 @@ abstract class APIConnection implements Serializable {
         public void onSuccess(String msg);
 
 
+        public void onSuccess(JSONArray jsonArray);
+
+        public void onSuccess(JSONObject jsonObject);
+
         /**
          * 失敗
          *
@@ -25,4 +35,25 @@ abstract class APIConnection implements Serializable {
         public void onFailed(String error);
     }
 
+    public void request(String url, final ConnectionListener listener) {
+        URLConnectionAsyncTask urlConnectionAsyncTask = new URLConnectionAsyncTask(){
+            @Override
+            protected void onPostExecute(JSONArray result) {
+                try {
+                    listener.onSuccess(result);
+                }catch (Exception e){
+                    listener.onFailed(e.getMessage());
+                }
+            }
+        };
+        urlConnectionAsyncTask.execute(url);
+    }
 }
+//try {
+//        if (result != null){
+//        listener.onSuccess(result);
+//        }
+//        }
+//        catch (JSONException e){
+//        listener.onFailed(e.getMessage());
+//        }
