41bd7fad51135b11207a98b556c07582dfafcc98
Ryu Hanaoka <doradora56@gmail.com>
2015-06-29 16:39:10 +0900

    [add] get操作追加

--- app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/home/HomeSelectEvent.java
+++ app/src/main/java/com/sleepingdragon/joko4nen/nosmoke/home/HomeSelectEvent.java
@@ -1,23 +1,56 @@
 package com.sleepingdragon.joko4nen.nosmoke.home;
 
-import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by ryu on 15/06/26.
  */
 public class HomeSelectEvent {
     private boolean success;
-    private JSONArray array;
+    private JSONObject object;
 
-    public HomeSelectEvent(boolean success, JSONArray array) {
+    public HomeSelectEvent(boolean success, JSONObject result) {
         this.success = success;
-        this.array = array;
+        this.object = result;
     }
     public boolean isSuccess() {
         return success;
     }
 
-    public JSONArray getJsonObject() {
-        return array;
+    public JSONObject getJsonObject() {
+        return object;
+    }
+
+    public String getUserId() throws JSONException {
+        return object.getString("UserId");
+    };
+
+    public String getUserName() throws JSONException {
+        return object.getString("Name");
+    }
+
+    public String getTeamName() throws JSONException {
+        return object.getString("TeamName");
+    }
+
+    public String getTeamStartDate() throws JSONException {
+        return object.getString("TeamStartDate");
+    }
+
+    public String getTeamFinalDate() throws JSONException {
+        return object.getString("TeamFinalDate");
+    }
+
+    public String getDeadline() throws JSONException {
+        return object.getString("Deadline");
+    }
+
+    public String getSmokinghistoryPerformanceNumberTeamSum() throws JSONException {
+        return object.getString("SmokinghistoryPerformanceNumberTeamSum");
+    }
+
+    public String getCigaretteNumber() throws JSONException {
+        return object.getString("CigaretteNumber");
     }
 }
